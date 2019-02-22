d79d44499b1d65a8185369c9f1fb5b8292787309
reismarcos <reismarcos@id.uff.br>
2019-02-22 18:31:50 -0300

    form - parte I pronta

--- src/app/formulario/formulario.page.ts
+++ src/app/formulario/formulario.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { FormularioService, Formulario } from '../services/formulario.service';
 import { Router } from '@angular/router';
 import { PacienteService } from '../services/paciente.service';
@@ -15,16 +15,96 @@ export class FormularioPage implements OnInit {
   question = 0;
   showInter = 0;
   showOptions = 0;
-  showInput = 0;
-  Q2_1A:boolean; Q2_1B:boolean; Q2_1C:boolean; Q2_1D:boolean; Q2_1E:boolean; 
-  I1_A:boolean; I1_B:boolean; I1_C:boolean;
+  
+  //Variaveis para checkbox
+  I2_1A:boolean; I2_1B:boolean; I2_1C:boolean; I2_1D:boolean; I2_1E:boolean; I2_1F:boolean; I2_1i; 
+
+  //Variaveis para checkbox de intervenção
+  IR1_A:boolean; IR1_B:boolean; IR1_C:boolean;
+  IR2_A:boolean; IR2_B:boolean;
+  IR3_A:boolean; IR3_B:boolean; IR3_C:boolean;
+  
 
   formulario: Formulario = {
-    paciente: '',
-    Q1: '',
-    Q2: '',
-    Q2_1:[],
-    I1:[],
+    cuidador: '',
+    // Variaveis relacionadas a parte I
+    I1: '',
+    I2: '',
+    I2_1:[],
+    I2_2: '',
+    IR1: [],
+    I3: '',
+    I4: '',
+    I5: '',
+    I5_1:'',
+    I6: '',
+    IR2: [],
+    I7: '',
+    IR3: [],
+    
+    // Variaveis relacionadas a parte II
+    II1: '',
+    II2: '',
+    II3: '',
+    II4: '',
+    II5: '',
+    II6:[],
+    II7: '',
+    II8: '',
+    IIR1:[],
+  
+    // Variaveis relacionadas a parte III
+    III1: '',
+    III1_1: [],
+    III2:'',
+    III2_1: '',
+    IIIR1: [],
+    III3: '',
+    III3_1: [],
+    IIIR2: [],
+    III4: '',
+    III4_1: '',
+    IIIR3: [],
+    III5: '',
+    III6: '',
+    IIIR4: [],
+    III7: '',
+    III7_1: '',
+    IIIR5: [],
+    III8:'',
+    III9:'',
+    III9_1:'',
+    III9_2: '',
+    IIIR6: [],
+    III10: '',
+    IIIR7: [],
+    III11:'',
+    III11_1: '',
+    IIIR8:[],
+    III12: '',
+    III12_1: [],
+    III12_2: '',
+    III13: '',
+    IIIR9: [],
+    
+    // Variaveis relacionadas a parte IV
+    IV1: '',
+    IV1_1: '',
+    IV2: '',
+    IV3: '',
+    IV3_1: '',
+    IV4: '',
+    IV4_1: '',
+    IVR1: [],
+    IV5: '',
+    IVR2: [],
+  
+    // Variaveis relacionadas a parte V
+    V1: '',
+    V2: '',
+    V2_1: '',
+    VR1: [],
+    V3: '',
   };
 
   formularioId = null;
@@ -40,6 +120,9 @@ export class FormularioPage implements OnInit {
   }
 
   public setQuestion(q){
+    if (this.showInter == 1){
+      this.showInter = 0;
+    }
     this.question = q;
   }
 
@@ -64,16 +147,28 @@ export class FormularioPage implements OnInit {
  public checkboxTraslate(){
 
   /* Checkbox Questao 2.1 */
-  if (this.Q2_1A) { this.formulario.Q2_1.push('Tomar banho') };
-  if (this.Q2_1B) { this.formulario.Q2_1.push('Higiene pessoal: usar o banheiro, limpar-se ou arrumar as roupas') };
-  if (this.Q2_1C) { this.formulario.Q2_1.push('Vestir-se') };
-  if (this.Q2_1D) { this.formulario.Q2_1.push('Alimentar-se ') };
-  if (this.Q2_1E) { this.formulario.Q2_1.push('Transferência: Levantar-se e andar') };
+  if (this.I2_1A) { this.formulario.I2_1.push('Tomar banho') };
+  if (this.I2_1B) { this.formulario.I2_1.push('Higiene pessoal: usar o banheiro, limpar-se ou arrumar as roupas') };
+  if (this.I2_1C) { this.formulario.I2_1.push('Vestir-se') };
+  if (this.I2_1D) { this.formulario.I2_1.push('Alimentar-se ') };
+  if (this.I2_1E) { this.formulario.I2_1.push('Transferência: Levantar-se e andar') };
+  if (this.I2_1F) { this.formulario.I2_1.push(this.I2_1i)};
 
-   /* Checkbox Intervenção 1 */
-  if (this.I1_A) { this.formulario.I1.push('Verificar a necessidade de dispositivos de adaptação para higiene pessoal, vestir-se, arrumar-se, usar o vaso sanitário e alimentar-se.') };
-  if (this.I1_B) { this.formulario.I1.push('Encorajar o idoso a realizar atividades normais da vida diária de acordo como seu nível de capacidade.')};
-  if (this.I1_C) { this.formulario.I1.push('Incentivar o cuidador a encorajar a independência do idoso e a interferir apenas quando o paciente não conseguir executar algo')};
+  /* Checkbox Intervenção 1R1 - I2 */
+  if (this.IR1_A) { this.formulario.IR1.push('Verificar a necessidade de dispositivos de adaptação para higiene pessoal, vestir-se, arrumar-se, usar o vaso sanitário e alimentar-se.') };
+  if (this.IR1_B) { this.formulario.IR1.push('Encorajar o idoso a realizar atividades normais da vida diária de acordo como seu nível de capacidade.')};
+  if (this.IR1_C) { this.formulario.IR1.push('Incentivar o cuidador a encorajar a independência do idoso e a interferir apenas quando o paciente não conseguir executar algo')};
+
+  /* Checkbox Intervenção IR2 - I6 */
+  if (this.IR2_A) { this.formulario.IR2.push("Ensinar ao cuidador os planos médicos e de enfermagem para o cuidado")};
+  if (this.IR2_B) { this.formulario.IR2.push("Determinar as expectativas comportamentais adequadas ao estado cognitivo do idoso")};
+
+  /* Checkbox Intervenção 1R3 - I7 */
+  if (this.IR3_A) { this.formulario.IR3.push('Avaliar o nível de conhecimento do cuidador ') };
+  if (this.IR3_B) { this.formulario.IR3.push('Orientar sobre a patologia – Explicar que o Alzheimer é predominante nos idosos, o cuidador deve ser orientado que a perda das funções cognitivas como a memória, atenção, linguagem e orientação são esperadas. Deve-se mencionar também que não há cura, mas há tratamento para a doença, o tratamento farmacológico e o não farmacológico que deve ser  composto por atividades de estimulação cognitiva, física e social visando a manutenção\ preservação dessas habilidades.  ')};
+  if (this.IR3_C) { this.formulario.IR3.push('Esclarecer dúvidas de acordo com a demanda')};
+
+
  }
 
 }
--- src/app/services/formulario.service.ts
+++ src/app/services/formulario.service.ts
@@ -4,11 +4,92 @@ import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators';
 
 export interface Formulario {
-  paciente: String;
-  Q1: String;
-  Q2: String;
-  Q2_1:String[];
-  I1: String[];
+  // I - V >>> RELACIONADO A PERGUNTAS
+  // R >>> RELACIONADO A INTERVENÇÃO
+  // ION-RADIO >>> Salva como String
+  // CHECKBOX >>> Salva como Array
+  // INPUT >>> Salva como String
+
+  cuidador: String;
+  // Variaveis relacionadas a parte I
+  I1: String;
+  I2: String;
+  I2_1:String[];
+  I2_2: String;
+  IR1: String[];
+  I3: String;
+  I4: String;
+  I5: String;
+  I5_1: String;
+  I6: String;
+  IR2: String[];
+  I7: String;
+  IR3: String[];
+  
+  // Variaveis relacionadas a parte II
+  II1: String;
+  II2: String;
+  II3:String;
+  II4: String;
+  II5: String;
+  II6:String[];
+  II7: String;
+  II8: String;
+  IIR1:String[];
+
+  // Variaveis relacionadas a parte III
+  III1: String;
+  III1_1: String[];
+  III2:String;
+  III2_1: String;
+  IIIR1: String[];
+  III3: String;
+  III3_1: String[];
+  IIIR2: String[];
+  III4: String;
+  III4_1: String;
+  IIIR3: String[];
+  III5: String;
+  III6: String;
+  IIIR4: String[];
+  III7: String;
+  III7_1: String;
+  IIIR5: String[];
+  III8:String;
+  III9:String;
+  III9_1:String;
+  III9_2: String;
+  IIIR6: String[];
+  III10: String;
+  IIIR7: String[];
+  III11:String;
+  III11_1: String;
+  IIIR8:String[];
+  III12: String;
+  III12_1: String[];
+  III12_2: String;
+  III13: String;
+  IIIR9: String[];
+  
+  // Variaveis relacionadas a parte IV
+  IV1: String;
+  IV1_1: String;
+  IV2: String;
+  IV3: String;
+  IV3_1: String;
+  IV4: String;
+  IV4_1: String;
+  IVR1: String[];
+  IV5: String;
+  IVR2: String[];
+
+  // Variaveis relacionadas a parte V
+  V1: String;
+  V2: String;
+  V2_1: String;
+  VR1: String[];
+  V3: String;
+
 }
 
 @Injectable({
