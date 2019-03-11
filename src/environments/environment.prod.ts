c7075ae2ea95f36758266ab8ea36f1b94d6839b8
Caio Guimarães <caio23guimaraes@gmail.com>
2019-03-11 13:29:25 -0300

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB0uHvabUJYcOQXw8ad9URu3guRzdmbfG4",
+    authDomain: "appteleidoso.firebaseapp.com",
+    databaseURL: "https://appteleidoso.firebaseio.com",
+    projectId: "appteleidoso",
+    storageBucket: "appteleidoso.appspot.com",
+    messagingSenderId: "661533482228"
+  }
 };
